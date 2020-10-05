fe6f38e6e69fdea52f97d7c971cf7a15021ebba3
Olle Bergkvist <ollebergkvist@gmail.com>
2020-10-05 11:19:14 -0500

    Updated socket-io server functionality

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Dotenv
 require("dotenv").config(); // Requires and configs dotenv
 
 // Express server
@@ -7,22 +8,49 @@ const app = express();
 // Socket.io
 const server = require("http").createServer(app);
 const io = require("socket.io")(server);
-
 io.origins([process.env.SOCKET_ORIGIN]); // Allows defined client to communicate with server
+
+// Dateformat
+var dateFormat = require("dateformat");
+
+// Store messages and users
+var messages = [];
+var users = [];
 
 // Creates socket.io connection
 io.on("connection", (socket) => {
     // Listens on connection event
     console.info("a user connected"); // Prints connection message event
 
-    // Listens on disconnect event
-    socket.on("disconnect", () => {
-        console.log("user disconnected"); // Prints chat message event
+    socket.emit("init-chat", messages); // When user connects, the server sends the user all the current messages
+    socket.emit("update-users", users); // When user connects, the server send the user a list with current users
+
+    // When user connects. The server updates user list and emits an event
+    // Listens on add-user event
+    socket.on("add-user", (user) => {
+        users.push({ id: socket.id, name: user });
+        io.emit("update-users", users);
     });
 
+    // When user disconnects from the server, user list updates and emits an event
+    // Listens to dicsonnect event
+    socket.on("disconnect", () => {
+        users = users.filter(function (user) {
+            return user.id != socket.id;
+        });
+        io.emit("update-users", users);
+    });
+
+    // When a user sends a message, server pushes the info to message list and emits an event
     // Listens on message event
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg); // Prints disconnection message event
+    socket.on("send-message", (data) => {
+        let newMessage = {
+            text: data.message,
+            user: data.user,
+            date: dateFormat(new Date(), "default"),
+        };
+        messages.push(newMessage);
+        io.emit("read-message", newMessage);
     });
 });
 
